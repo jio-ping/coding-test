f50d04bf8ce084e69f064c87dbe36ec66f1eb746
jio <pennicillium@daum.net>
2023-06-13 15:17:20 +0900

    23-06-13

--- 120845.py
+++ 120845.py
@@ -1,8 +1,3 @@
-"""공던지기"""
-def solution(numbers,k):
-    number_list = numbers*(1000//len(numbers))
-    answer = []
-    for i in range(0,2*k,2):
-        answer.append(number_list[i])
-    return answer[-1]
-print(solution([1,2,3],3))
+# """공던지기"""
+def solution(numbers, k):
+    return 2 * (k - 1) % numbers[-1] + 1
