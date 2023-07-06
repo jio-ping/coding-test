e5073244a9c7ac757d4501155fc41874b5be5b0d
jio <pennicillium@daum.net>
2023-07-06 22:55:35 +0900

    23-07-06

--- 120875.py
+++ 120875.py
@@ -1,12 +1,13 @@
 """평행"""
 def solution(dots):
-    if ((dots[0][1] - dots[1][1])  //( dots[0][0] - dots[1][0]) ) == (( dots[2][1] - dots[3][1] )//( dots[2][0] - dots[3][0] )):
-        return 1
-    elif (( dots[0][1] - dots[2][1])  // (dots[0][0] - dots[2][0] )) == (( dots[1][1] - dots[3][1]) // (dots[1][0] - dots[3][0] )):
-        return 1
-    elif (( dots[0][1] - dots[3][1]) // (dots[0][0] - dots[3][0] )) == ((dots[1][1] - dots[2][1] )// (dots[1][0] - dots[2][0] )):
-        return 1
-    else:
-        return 0
+    answer = 0
 
-print(solution([[3, 5], [4, 1], [2, 4], [5, 10]]))
+    if (dots[0][0] - dots[1][0])/(dots[0][1] - dots[1][1]) == (dots[2][0] - dots[3][0])/(dots[2][1] - dots[3][1]): # 01 23
+        answer = 1
+    elif (dots[0][0] - dots[2][0])/(dots[0][1] - dots[2][1]) == (dots[1][0] - dots[3][0])/(dots[1][1] - dots[3][1]): # 02 13
+        answer = 1
+    elif (dots[0][0] - dots[3][0])/(dots[0][1] - dots[1][1]) == (dots[2][0] - dots[3][0])/(dots[2][1] - dots[3][1]): # 03 12
+        answer = 1
+    else:
+        answer = 0  
+    return answer
