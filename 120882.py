cde84f20a35188b2b14f49b510e236e348756aa1
jio <pennicillium@daum.net>
2023-08-04 21:59:13 +0900

    23-08-04

--- 120882.py
+++ 120882.py
@@ -1,4 +1,6 @@
 """등수매기기"""
 def solution(scores):
-    sum_score = [sum(score[0],score[1]) for score in scores]
+    sum_score = sorted([sum(score) for score in scores],reverse=True)
+    return [sum_score.index(sum(score))+1 for score in scores]
     
+print(solution([[80, 70], [70, 80], [30, 50], [90, 100], [100, 90], [100, 100], [10, 30]]))
