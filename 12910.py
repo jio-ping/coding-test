3395a9418d17192ad28c58b011311518b39c3b52
jio <pennicillium@daum.net>
2023-08-16 20:59:07 +0900

    23-08-16

--- 12910.py
+++ 12910.py
@@ -1,5 +1,14 @@
 # 나누어 떨어지는 숫자 배열
 def solution(arr,divisor):
-    ff = [ar for ar in arr if ar%divisor == 0]
-    return 
-print(solution([3,2,6],10))
+    answer = []
+    for ar in arr :
+        if ar%divisor==0:
+            answer.append(ar)
+    if len(answer)>0:
+        return sorted(answer)
+    else:
+        return [-1]
+print(solution([2, 36, 1, 3],1	))
+
+# 다른사람풀이
+def solution(arr, divisor): return sorted([n for n in arr if n%divisor == 0]) or [-1]
