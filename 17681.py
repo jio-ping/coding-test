2b396fe6ce6d29bdbada6c13fd5d25c02e59fac3
jio <pennicillium@daum.net>
2023-08-24 21:37:28 +0900

    23-08-24

--- 17681.py
+++ 17681.py
@@ -1,20 +1,21 @@
 # 비밀지도
-def solution(arr1,arr2,n):
-    binary_arr = []
-    answer =[]
-    for i in range(0,n):
-        tmp  = bin ( arr1[i] | arr2[i])[2:].zfill(n)
-        binary_arr.append(tmp)
-    for binary in binary_arr:
-        answer.append(binary.replace("0b","").replace("1","#").replace("0"," ").zfill(n))
+def solution(n, arr1, arr2):
+    answer = []
+    for i in range(n):
+        tmp = bin(arr1[i] | arr2[i])[2:].zfill(n)
+        tmp = tmp.replace("1", "#").replace("0", " ")
+        answer.append(tmp)
     return answer
     
 print(solution([9, 20, 28, 18, 11],[30, 1, 21, 17, 28],5))
 
+# 다른사람풀이
 def solution(n, arr1, arr2):
     answer = []
-    for i in range(n):
-        a = bin(arr1[i] | arr2[i])[2:].zfill(n)
-        b = a.replace("1", "#").replace("0", " ")
-        answer.append(b)
+    for i,j in zip(arr1,arr2):
+        a12 = str(bin(i|j)[2:])
+        a12=a12.rjust(n,'0')
+        a12=a12.replace('1','#')
+        a12=a12.replace('0',' ')
+        answer.append(a12)
     return answer
