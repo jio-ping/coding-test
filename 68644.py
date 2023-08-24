57da5065e4c5a1b31c8d5e2cf85d9ccc50c46eec
jio <pennicillium@daum.net>
2023-08-24 21:23:55 +0900

    23-08-24

--- 68644.py
+++ 68644.py
@@ -1,7 +1,15 @@
 # 두개뽑아서 더하기
-from itertools import combinations, permutations
-
+from itertools import combinations
 def solution(numbers):
-    return list(combinations(numbers,2))
+    comb_list =combinations(numbers,2)
+    return sorted(list(set([comb[0]+comb[1] for comb in comb_list])))
+        
+print(solution([1,1]))
 
-print(solution([2,1,3,4,1]))
+# 다른사람풀이
+def solution(numbers):
+    answer = []
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            answer.append(numbers[i] + numbers[j])
+    return sorted(list(set(answer)))
