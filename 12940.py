d7028dcc436e96b9518e95ec25d9d63c59f95fc1
jio <pennicillium@daum.net>
2023-08-27 22:29:21 +0900

    23-08-27

--- 12940.py
+++ 12940.py
@@ -1,14 +1,14 @@
 # 최대 공약수와 최소공배수
-def solution(n,m):
-    tmp = 1
-    for i in range(min(n,m),1,-1):  
-        if n%i ==0 and m%i==0:
-            print(i)
-            n=n//i
-            m=m//i
-            tmp *= i    
-    return ([tmp,int(tmp * n* m)])
-print(solution(76,36))        
+# def solution(n,m):
+#     tmp = 1
+#     for i in range(min(n,m),1,-1):  
+#         if n%i ==0 and m%i==0:
+#             print(i)
+#             n=n//i
+#             m=m//i
+#             tmp *= i    
+#     return ([tmp,int(tmp * n* m)])
+      
     
 # 다른사람풀이 
 
@@ -16,6 +16,7 @@ def solution(n, m):
     gcd = lambda a,b : b if not a%b else gcd(b, a%b)
     lcm = lambda a,b : a*b//gcd(a,b)
     return [gcd(n, m), lcm(n, m)]
+
 
 def gcdlcm(a, b):
     c,d = max(a, b), min(a, b)
@@ -25,3 +26,12 @@ def gcdlcm(a, b):
         c, d = d, t
     answer = [ c, int (a*b/c)]
     return answer
+
+def gcd(a,b):
+    print(a%b)
+    if not a%b:
+        return b
+    else:
+        gcd(b,a%b)
+
+print(gcd(76,36))
