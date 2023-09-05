eda1a00c7aa289b86d87310b36a0c7194eae3cc4
jio <pennicillium@daum.net>
2023-09-05 20:34:58 +0900

    23-09-05

---
+++ 161989.py
@@ -0,0 +1,28 @@
+# 덧칠하기
+def solution(n, m, section):
+    answer = 1 
+    paint = section[0]
+    for i in range(1, len(section)):
+        if section[i] - paint >= m:
+            answer += 1
+            paint = section[i]
+            
+    return answer
+
+print(solution(4,1,[1,2, 3, 4]))
+print("_____")
+print(solution( 4, 2, [3, 4]))
+print("_____")
+print(solution(8,4,[2, 3, 6]))
+print("_____")
+print(solution(5,5,[3]))
+
+# 시간초과
+def time_out_solution(n,m,section):
+    cnt  = 0
+    while len(section) > 1:
+        for i in range(min(section),max(section)+1,m):
+            if i in section:
+                section.remove(i)
+                cnt += 1
+    return cnt
