ea6700125325f1798587d311d8fdb1d29167862b
jio <pennicillium@daum.net>
2023-09-15 22:38:27 +0900

    23-09-15

---
+++ 131128.py
@@ -0,0 +1,6 @@
+# 숫자짝꿍
+def solution(X,Y):
+    answer = ""
+    a , b = (min(len(X),len(Y))) ,max(len(X,len(Y)))
+    for i in range(min(len(X),len(Y))):
+        len(X)
--- 133499.py
+++ 133499.py
@@ -4,10 +4,20 @@ def solution(babblings ):
     answer = []
     for babbling in babblings:
         for canspeak in can_speak:
-            if not babbling.startswith(f'{canspeak+canspeak}'):
+            if not (f'{canspeak+canspeak}') in babbling:
                 babbling=babbling.replace(canspeak,"1")
         answer.append(babbling)
     return len([ans for ans in answer if ans.isdigit()])
-print(solution(["ayayeayayeaya"]))
-# 1
-print(solution(  ["yemayemayemayemayemayemayemaye"]))
+print(solution(  ["ayayeye"]))
+
+# 다른사람풀이
+
+def solution(babbling):
+    answer = 0
+    for i in babbling:
+        for j in ['aya','ye','woo','ma']:
+            if j*2 not in i:
+                i=i.replace(j,' ')
+        if len(i.strip())==0:
+            answer +=1
+    return answer
