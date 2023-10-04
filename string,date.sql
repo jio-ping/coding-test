ba9680a4aca4d4536dee59380d27f59cc5241349
jio <pennicillium@daum.net>
2023-10-04 19:42:36 +0900

    23-10-04

--- string,date.sql
+++ string,date.sql
@@ -1,5 +1,26 @@
 --STRING & DATE 
-
+--51384
+SELECT
+    USER_ID,
+    NICKNAME,
+    CONCAT(CITY,' ',STREET_ADDRESS1,' ',STREET_ADDRESS2) AS '전체주소',
+    CONCAT(SUBSTR(TLNO,1,3),'-',SUBSTR(TLNO,4,4),'-',SUBSTR(TLNO,8,4)) AS '전화번호'
+FROM
+    USED_GOODS_USER
+WHERE
+    USER_ID IN
+            (
+            SELECT
+                WRITER_ID
+            FROM 
+                USED_GOODS_BOARD
+            GROUP BY
+                WRITER_ID
+            HAVING
+                COUNT(*) >= 3
+            )
+ORDER BY
+    USER_ID DES
 --59046
 --MYSQL&ORACLE
 SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
