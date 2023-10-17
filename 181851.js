4ec4e772d5bb99428c5a5a05cb175519da274e5c
jio <pennicillium@daum.net>
2023-10-17 18:25:23 +0900

    23-10-17

--- 181851.js
+++ 181851.js
@@ -1,19 +1,23 @@
 // 전국대회선발고사
 function solution(rank, attendance) {
-  for (let i = 0; i < rank.length; i++) {
+  for (let i = 0; i < attendance.length; i++) {
     if (attendance[i] == false) {
+      rank.remove(i);
     }
   }
+  rank.sort();
+  return rank[0] * 10000 + rank[1] + 100 + rank[2];
 }
+
 console.log(
   solution([3, 7, 2, 5, 4, 6, 1], [false, true, true, true, true, false, false])
 );
 
 //다른사람풀이
-function solution(rank, attendance) {
-  const [a, b, c] = rank
-    .map((r, i) => [r, i])
-    .filter(([_, i]) => attendance[i])
-    .sort(([a], [b]) => a - b);
-  return 10000 * a[1] + 100 * b[1] + c[1];
-}
+// function solution(rank, attendance) {
+//   const [a, b, c] = rank
+//     .map((r, i) => [r, i])
+//     .filter(([_, i]) => attendance[i])
+//     .sort(([a], [b]) => a - b);
+//   return 10000 * a[1] + 100 * b[1] + c[1];
+// }
