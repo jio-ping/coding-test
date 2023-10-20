a3fa2026656d79e88247d1ee89560a9608481344
jio <pennicillium@daum.net>
2023-10-20 23:26:01 +0900

    23-10-20

--- 181837.py
+++ 181837.py
@@ -1,4 +1,9 @@
 # 커피심부름
 def solution(orders):
+    sum = 0
     for order in orders:
-        if 
+        if "americano" in order or "anything" == order:
+            sum += 4500
+        elif "cafelatte" in order:
+            sum += 5000
+    return sum 
