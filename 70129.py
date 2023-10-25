e5120e52e0ceea7cf9893001045883d916dbe249
jio <pennicillium@daum.net>
2023-10-25 16:36:14 +0900

    "23-10-25"

--- 70129.py
+++ 70129.py
@@ -9,11 +9,21 @@
 def solution(n):
     cnt = 0 
     delete_zero = 0
-    if len(n) != "1":
-        print(n.count("0"))
-        delete_zero+= n.count("0")
-        cnt += 1
-        n = str(bin(len(n)-delete_zero))[2:]
-    print(n)
+    while 1:
+        if n != "1":
+            delete_zero+= n.count("0")
+            cnt += 1
+            n = str(bin(len(n)-delete_zero))[2:]
+        pass
+    return [cnt, delete_zero]
 
 print(solution("110010101001"))
+
+def solution(s):
+    a, b = 0, 0
+    while s != '1':
+        a += 1
+        num = s.count('1')
+        b += len(s) - num
+        s = bin(num)[2:]
+    return [a, b]
